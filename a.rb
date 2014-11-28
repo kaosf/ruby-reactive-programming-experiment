6a281e00875fc8901297305007230ce03671ed9e
ka <ka.kaosf@gmail.com>
2014-11-28 22:06:44 +0900

    Create a sample program
    ref. https://github.com/steveklabnik/frappuccino

--- a.rb
+++ a.rb
@@ -13,14 +13,14 @@ counter = stream
             .map {|event| event == :pushed ? 1 : 0 } # convert events to ints
             .inject(0) {|sum, n| sum + n } # add them up
 
-puts counter.now # => 0
+counter.now # => 0
 
 button.push
 button.push
 button.push
 
-puts counter.now # => 3
+counter.now # => 3
 
 button.push
 
-puts counter.now # => 4
+counter.now # => 4
